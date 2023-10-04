90ca626f4e916877ab22de77247f578136a1d4b4
LAUTARO FERNANDO YUKELSON <47434208@ort>
2023-10-04 12:14:33 -0300

    Vanilla JS Tetris

---
+++ Js/index.js
@@ -0,0 +1,233 @@
+const TAMAÑO_BLOQUE = 20;
+const ANCHO = 10;
+const ALTO = 20;
+
+const piezas = [
+    {
+        posision: {x: 8, y: 0},
+        shape: [
+            [1,1],
+            [1,1],
+        ]
+    },
+    {
+        posision: {x: 0, y: 0},
+        shape: [
+            [1,0],
+            [1,0],
+            [1,1]
+        ]
+    },
+    {
+        posision: {x: 0, y: 0},
+        shape: [
+            [0,1,0],
+            [1,1,1],
+        ]
+    },
+    {
+        posision: {x: 0, y: 0},
+        shape: [
+            [0,1],
+            [0,1],
+            [1,1]
+        ]
+    },
+    {
+        posision: {x: 0, y: 0},
+        shape: [
+            [1],
+            [1],
+            [1],
+            [1]
+        ]
+    },
+    {
+        posision: {x: 0, y: 0},
+        shape: [
+            [1,1,0],
+            [0,1,1],
+        ]
+    },
+    {
+        posision: {x: 0, y: 0},
+        shape: [
+            [0,1,1],
+            [1,1,0],
+        ]
+    }
+
+];
+
+const canvas = document.getElementById("gameCanvas");
+const context = canvas.getContext("2d");
+const puntos = document.getElementById("puntos");
+let puntaje = 0;
+let tablero = [];
+let bajando = false;
+
+canvas.width = TAMAÑO_BLOQUE * ANCHO;
+canvas.height = TAMAÑO_BLOQUE * ALTO;
+context.scale(TAMAÑO_BLOQUE, TAMAÑO_BLOQUE);
+
+function crearTablero(){
+    for (let i = 0; i<ALTO; i++){
+        let columna = [];
+        for (let x = 0; x<ANCHO; x++){
+            columna.push(0);
+        }
+        tablero.push(columna);
+    }
+    
+}
+
+function proxPieza(){
+    pieza = piezas[Math.floor(Math.random() * piezas.length)];
+    pieza.posision.x = Math.floor(ANCHO / 2);
+    pieza.posision.y = 0;
+    if(verColision()){
+        perdiste();
+    }
+}
+
+function actualizar(context){
+    dibujar();
+    window.requestAnimationFrame(actualizar);
+}
+
+function dibujar(){
+    context.fillStyle = "black";
+    context.fillRect(0,0, canvas.width, canvas.height);
+    tablero.forEach((row, y) => {
+        row.forEach((value, x) => {
+            if (value === 1){
+                context.fillStyle = "#b8e3d2";
+                context.fillRect(x, y, 1, 1);
+            }
+        })
+    })
+
+    pieza.shape.forEach((row, y) => {
+        row.forEach((value, x) => {
+            if (value === 1){
+                context.fillStyle = "red";
+                context.fillRect(x + pieza.posision.x, y + pieza.posision.y, 1, 1);
+            }
+        })
+    })
+}
+
+function verColision(){
+    return pieza.shape.find((row, y) => {
+        return row.find((value, x) => {
+            return (
+                value == 1 &&
+                tablero[y + pieza.posision.y]?.[x + pieza.posision.x] != 0
+            )
+        });
+    });
+}
+
+function endurecer(){
+    pieza.shape.forEach((row, y) => {
+        row.forEach((value, x) => {
+            if (value === 1){
+                tablero[y + pieza.posision.y][x + pieza.posision.x] = 1;
+            }
+        })
+    })
+}
+
+function eliminarFilas(){
+    tablero.forEach((row, y) =>{
+        if (row.every((element) => element === 1)){
+            tablero.splice(y, 1);
+            const ceroArray = Array.apply(null, Array(ANCHO)).map(() => 0);
+            tablero.unshift(ceroArray);
+            puntaje += 100;
+            puntos.innerText = puntaje;
+        }
+    });
+}
+
+function LEFT(){
+    pieza.posision.x--;
+    if (verColision()){
+        pieza.posision.x++;
+    }
+}
+
+function RIGHT(){
+    pieza.posision.x++;
+    if (verColision()){
+        pieza.posision.x--;
+    }
+}
+
+function UP(){
+    const anteriorPieza = pieza.shape;
+    let vuelta = [];
+    for (let i = 0; i<pieza.shape[0].length; i++){
+        let fila = [];
+        for (let x = pieza.shape.length - 1; x >= 0; x--){
+            fila.push(pieza.shape[x][i]);
+        }
+        vuelta.push(fila);
+    }
+    pieza.shape = vuelta;
+    if (verColision()){
+        pieza.shape = anteriorPieza;
+    }
+}
+
+function DOWN(){
+    pieza.posision.y++;
+    if (verColision()){
+        pieza.posision.y--;
+        endurecer();
+        bajando = false;
+        eliminarFilas();
+        proxPieza();
+    }
+}
+
+function perdiste(){
+    const gameContainer = document.getElementById("gameContainer");
+    gameContainer.style.display = "none";
+
+    const perdiste = document.getElementById("perdiste");
+    perdiste.style.display = "flex";
+    perdiste.addEventListener("click", () => {
+        location.reload();
+    })
+}
+
+document.addEventListener("keydown", (event) => {
+    switch(event.keyCode){
+        case 37:
+            LEFT();
+            break;
+        case 39:
+            RIGHT();
+            break;
+        case 38:
+            UP();
+            break;
+        case 40:
+            DOWN();
+            break;
+        case 32:
+            bajando = true;
+            while(bajando){
+                DOWN();
+            }
+            break;
+        default:
+            break;
+    }
+});
+
+crearTablero();
+let pieza = piezas[0];
+let interval = setInterval(DOWN, 1000);
+actualizar();
